0974b889a91e732facad4679d0fec25fc97ccf53
UnsignedArduino <UnsignedArduino@outlook.com>
2021-04-02 12:01:35 -0400

    Include default stuff in Python package

---
+++ TkZero/__init__.py
@@ -0,0 +1,8 @@
+"""
+TkZero - a sane and Pythonic wrapper around Tkinter.
+"""
+
+__version_info__ = (0, 0, 1)
+__version__ = "".join([str(p) for p in __version_info__])
+__status__ = "Prototype"
+__author__ = "UnsignedArduino at https://github.com/UnsignedArduino"
