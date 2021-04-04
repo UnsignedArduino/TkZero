47bc91034dd724c88c60760d04c15ae158e6f85b
UnsignedArduino <UnsignedArduino@outlook.com>
2021-04-04 11:48:52 -0400

    Update __init__.py

--- TkZero/__init__.py
+++ TkZero/__init__.py
@@ -3,6 +3,11 @@ TkZero - a sane and Pythonic wrapper around Tkinter.
 """
 
 __version_info__ = (0, 0, 1)
-__version__ = "".join([str(p) for p in __version_info__])
+__version__ = ""
+for index, digit in enumerate(__version_info__):
+    __version__ += str(digit)
+    if index < len(__version_info__) - 1:
+        __version__ += "."
+# "Prototype", "Development", or "Production"
 __status__ = "Prototype"
 __author__ = "UnsignedArduino at https://github.com/UnsignedArduino"
