4df0e7483786727430de0852e1e9c996dafd05da
UnsignedArduino <UnsignedArduino@outlook.com>
2021-04-14 18:23:59 -0400

    Add spinbox

---
+++ Tests/test_Spinbox.py
@@ -0,0 +1,80 @@
+import unittest
+from TkZero.MainWindow import MainWindow
+from TkZero.Spinbox import Spinbox
+from TkZero import Style
+
+
+class SpinboxTest(unittest.TestCase):
+    def test_no_params(self):
+        root = MainWindow()
+        root.update()
+        with self.assertRaises(TypeError):
+            Spinbox()
+        root.update()
+        root.close()
+
+    def test_good_params(self):
+        root = MainWindow()
+        root.update()
+        Spinbox(root, width=20, show="*", values=("foo", "bar")).grid(row=0, column=0)
+        root.update()
+        root.close()
+
+    def test_value(self):
+        root = MainWindow()
+        root.update()
+        s = Spinbox(root)
+        s.grid(row=0, column=0)
+        root.update()
+        self.assertEqual(s.value, "")
+        s.value = "Foo"
+        self.assertEqual(s.value, "Foo")
+        root.close()
+
+    def test_values(self):
+        root = MainWindow()
+        root.update()
+        s = Spinbox(root)
+        s.grid(row=0, column=0)
+        root.update()
+        self.assertEqual(s.values, ())
+        s.values = ("Foo", )
+        self.assertEqual(s.values, ("Foo", ))
+        root.close()
+
+    def test_enabled(self):
+        root = MainWindow()
+        root.update()
+        s = Spinbox(root)
+        s.grid(row=0, column=0)
+        root.update()
+        self.assertTrue(s.enabled)
+        s.enabled = False
+        self.assertFalse(s.enabled)
+        root.close()
+
+    def test_read_only(self):
+        root = MainWindow()
+        root.update()
+        s = Spinbox(root)
+        s.grid(row=0, column=0)
+        root.update()
+        self.assertFalse(s.read_only)
+        s.read_only = True
+        self.assertTrue(s.read_only)
+        root.close()
+
+    def test_style(self):
+        root = MainWindow()
+        root.update()
+        s = Spinbox(root)
+        s.grid(row=0, column=0)
+        Style.define_style(Style.WidgetStyleRoots.Button, "Test", background="red")
+        s.apply_style("Test")
+        self.assertEqual(s.cget("style"), "Test.TSpinbox")
+        root.update()
+        root.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ TkZero/Spinbox.py
@@ -0,0 +1,245 @@
+"""
+Creates a themed Spinbox.
+"""
+import tkinter as tk
+from tkinter import ttk
+from TkZero.Platform import on_aqua
+from pathlib import Path
+from PIL.ImageTk import PhotoImage
+from typing import Union, Any, Callable
+
+
+class Spinbox(ttk.Spinbox):
+    def __init__(self, parent: Union[tk.Widget, Union[tk.Tk, tk.Toplevel]], values: tuple[str, ...] = (), width: int = None,
+                 show: str = None, validate: Callable = None, command: Callable = None):
+        """
+        Initiate a ttk.Spinbox.
+
+        :param parent: The parent of the combobox.
+        :param values: The values you can choose, should be a tuple of str. Defaults to ()
+        :param width: The width of the combobox. Defaults to None.
+        :param show: The character to show instead of the actual text. Defaults to None.
+        :param validate: The function to use for validation. Will be passed in a positional argument with the text as
+         a str (like `validate(contents)`) and should return a bool - True if passed otherwise False.
+        :param command: The command to run when the value of the combobox changes. Defaults to None.
+        """
+        self._variable = tk.StringVar(value="")
+        if command is not None:
+            self._variable.trace_add("write", lambda *args: command())
+        super().__init__(master=parent, width=width, textvariable=self._variable, show=show,
+                         validatecommand=(parent.register(validate), "%p") if validate is not None else None)
+        self._style_root = "TSpinbox"
+        self._enabled = True
+        self._readonly = False
+        if on_aqua(self):
+            self.bind("<2>", lambda event: self._popup(event=event))
+            self.bind("<Control-1>", lambda event: self._popup(event=event))
+        else:
+            self.bind("<3>", lambda event: self._popup(event=event))
+        if values is not None:
+            self["values"] = values
+
+    @property
+    def value(self) -> str:
+        """
+        Get the text in this spinbox.
+
+        :return: A str of the text in this entry.
+        """
+        return self.get()
+
+    @value.setter
+    def value(self, new_text: str) -> None:
+        """
+        Set the text on this spinbox.
+
+        :param new_text: The new text.
+        :return: None.
+        """
+        if not isinstance(new_text, str):
+            raise TypeError(f"new_text is not a str! (type passed in: {repr(type(new_text))})")
+        self.delete(0, tk.END)
+        self.insert(0, new_text)
+
+    @property
+    def values(self) -> tuple[str]:
+        """
+        Get the options you can select.
+
+        :return: A str of the text in this entry.
+        """
+        return self["values"] if self["values"] else ()
+
+    @values.setter
+    def values(self, new_values: tuple[str]) -> None:
+        """
+        Set the options text on this spinbox.
+
+        :param new_values: The new options, as a tuple of str.
+        :return: None.
+        """
+        if not isinstance(new_values, tuple):
+            raise TypeError(f"new_values is not a tuple! (type passed in: {repr(type(new_values))})")
+        self["values"] = new_values
+
+    @property
+    def enabled(self) -> bool:
+        """
+        Whether this widget is disabled (grayed out and cannot interact with) in Tk terms.
+
+        :return: A bool, True if normal otherwise False.
+        """
+        return self._enabled
+
+    @enabled.setter
+    def enabled(self, new_state: bool) -> None:
+        """
+        Set whether this widget is in normal mode or disabled mode (grayed out and cannot interact with) in Tk terms.
+
+        :param new_state: The new state (a bool) True for enabled and False for disabled.
+        :return: None.
+        """
+        if not isinstance(new_state, bool):
+            raise TypeError(f"new_state is not a bool! (type passed in: {repr(type(new_state))})")
+        self._enabled = new_state
+        self._readonly = False
+        self.state(["!disabled" if self._enabled else "disabled"])
+
+    @property
+    def read_only(self) -> bool:
+        """
+        Whether this widget is read only (cannot type in selection manually - must select from pre-defined values) in
+        Tk terms.
+
+        :return: A bool, True if read only otherwise False.
+        """
+        return self._readonly
+
+    @read_only.setter
+    def read_only(self, new_state: bool) -> None:
+        """
+        Set whether this widget is in normal mode or read only mode (cannot type in selection manually - must select
+        from pre-defined values) in Tk terms.
+
+        :param new_state: The new state (a bool) True for normal and False for read only.
+        :return: None.
+        """
+        if not isinstance(new_state, bool):
+            raise TypeError(f"new_state is not a bool! (type passed in: {repr(type(new_state))})")
+        self._enabled = True
+        self._readonly = new_state
+        self.state(["readonly" if self._readonly else "!readonly"])
+
+    def _make_context_menu(self) -> None:
+        """
+        Initiate the context menu.
+
+        :return: None.
+        """
+        self._context_menu = tk.Menu(self, tearoff=0)
+        self._context_menu.add_command(label="Copy", command=self.copy_contents, underline=0,
+                                       accelerator="Command-C" if on_aqua(self) else "Control+C")
+        self._context_menu.add_command(label="Cut", command=self.cut_contents,
+                                       accelerator="Command-X" if on_aqua(self) else "Control+X")
+        self._context_menu.add_command(label="Paste", command=self.paste_contents, underline=0,
+                                       accelerator="Command-V" if on_aqua(self) else "Control+V")
+        self._context_menu.add_separator()
+        self._context_menu.add_command(label="Delete", command=self.delete_contents,
+                                       accelerator="Delete")
+        self._context_menu.add_separator()
+        self._context_menu.add_command(label="Select all", command=self.select_all_contents, underline=7,
+                                       accelerator="Command-A" if on_aqua(self) else "Control+A")
+
+    def _update_context_menu_states(self) -> None:
+        """
+        Update the context menu states, like whether to allow copying or not.
+
+        :return: None.
+        """
+        if self.selection_present():
+            self._context_menu.entryconfigure("Copy", state=tk.NORMAL)
+            self._context_menu.entryconfigure("Cut", state=tk.NORMAL)
+            self._context_menu.entryconfigure("Paste", state=tk.NORMAL)
+            self._context_menu.entryconfigure("Delete", state=tk.NORMAL)
+            self._context_menu.entryconfigure("Select all", state=tk.DISABLED)
+        else:
+            self._context_menu.entryconfigure("Copy", state=tk.DISABLED)
+            self._context_menu.entryconfigure("Cut", state=tk.DISABLED)
+            self._context_menu.entryconfigure("Paste", state=tk.NORMAL)
+            self._context_menu.entryconfigure("Delete", state=tk.DISABLED)
+            self._context_menu.entryconfigure("Select all", state=tk.NORMAL)
+
+    def _popup(self, event) -> None:
+        """
+        Try to pop up the context menu.
+
+        :param event: An object that Tk passes in with information about the event that we need.
+        :return: None.
+        """
+        self._update_context_menu_states()
+        if self.enabled:
+            try:
+                self._context_menu.tk_popup(event.x_root, event.y_root, 0)
+            finally:
+                self._context_menu.grab_release()
+
+    def select_all_contents(self) -> None:
+        """
+        Select everything.
+
+        :return: None.
+        """
+        self.select_range(0, tk.END)
+
+    def delete_contents(self) -> None:
+        """
+        Delete the highlighted things.
+
+        :return: None.
+        """
+        if self.selection_present():
+            self.delete(tk.SEL_FIRST, tk.SEL_LAST)
+
+    def paste_contents(self) -> None:
+        """
+        Paste into the entry.
+
+        :return: None.
+        """
+        self.delete_contents()
+        self.insert(self.index(tk.INSERT), self.clipboard_get())
+
+    def cut_contents(self) -> None:
+        """
+        Cut the highlighted contents.
+
+        :return: None.
+        """
+        if self.selection_present():
+            self.clipboard_clear()
+            self.clipboard_append(self.selection_get())
+            self.update()
+            self.delete(tk.SEL_FIRST, tk.SEL_LAST)
+            self.select_clear()
+
+    def copy_contents(self) -> None:
+        """
+        Copy the highlighted contents.
+
+        :return: None.
+        """
+        if self.selection_present():
+            self.clipboard_clear()
+            self.clipboard_append(self.selection_get())
+            self.update()
+
+    def apply_style(self, style_name: str) -> None:
+        """
+        Apply a theme to this combobox.
+
+        :param style_name: The name of the theme as a str, ex. "Warning"
+        :return: None.
+        """
+        if not isinstance(style_name, str):
+            raise TypeError(f"style_name is not a str! (type passed in: {repr(type(style_name))})")
+        self.configure(style=f"{style_name}.{self._style_root}")
