348103f01b3666cd21ebc4ae247674810eb0f190
UnsignedArduino <UnsignedArduino@outlook.com>
2021-04-18 19:16:19 -0400

    Make label example

---
+++ Examples/Widgets/Label/Label.py
@@ -0,0 +1,28 @@
+from TkZero.Label import Label
+from TkZero.MainWindow import MainWindow
+from PIL.ImageTk import PhotoImage
+from datetime import datetime
+
+# Make the main window and give it a title
+root = MainWindow()
+root.title = "Simple Label Example"
+
+# Make a simple label that shows an image
+clock_img = Label(root, image=PhotoImage(file="clock.png"))
+clock_img.grid(row=0, column=0)
+
+# Make a label that shows the time
+time_label = Label(root)
+time_label.grid(row=0, column=1)
+
+
+# Define a function to update the time
+def update_time():
+    time_label.text = str(datetime.now().strftime("%A, %B %d, %Y\n%H:%M:%S:%f"))
+    root.after(100, update_time)
+
+
+update_time()
+
+# Start the mainloop like in Tkinter
+root.mainloop()
