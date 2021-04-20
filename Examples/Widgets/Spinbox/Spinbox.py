401d2210afe610e0cb957410c63b1b138580aab5
UnsignedArduino <UnsignedArduino@outlook.com>
2021-04-20 16:39:09 -0400

    Make spinbox example (which is same as combobox)

---
+++ Examples/Widgets/Spinbox/Spinbox.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+
+from TkZero.Button import Button
+from TkZero.Spinbox import Spinbox
+from TkZero.Label import Label
+from TkZero.MainWindow import MainWindow
+
+# Make the main window and give it a title
+root = MainWindow()
+root.title = "Simple Spinbox Example"
+
+# Make a label for the combobox
+lang_label = Label(root, text="Select year: ")
+lang_label.grid(row=0, column=0)
+
+# Make a combobox
+lang_box = Spinbox(root)
+lang_box.grid(row=0, column=1)
+lang_box.values = tuple(range(1900, 2022, 1))
+lang_box.value = "2021"
+
+
+# "Submit" the year
+def submit():
+    # You can enable or disable the combobox with combobox.enabled
+    lang_box.enabled = False
+    submit_btn.enabled = False
+    # You can get (and set) the value of the combobox with combobox.value
+    print(lang_box.value)
+
+
+# Make a button that "submits" the year
+submit_btn = Button(root, text="Submit", command=submit)
+submit_btn.grid(row=1, column=0, columnspan=2, sticky=tk.NSEW)
+
+# Start the mainloop like in Tkinter
+root.mainloop()
