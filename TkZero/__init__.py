c0f27ceacd5f7e6a2dd71e68bd5b530ed8bcf013
UnsignedArduino <UnsignedArduino@outlook.com>
2021-04-22 16:24:32 -0400

    Update TODOs

--- TkZero/__init__.py
+++ TkZero/__init__.py
@@ -1,11 +1,6 @@
 """
 TkZero - a sane and Pythonic wrapper around Tkinter.
 """
-# TODO: Make examples of "full-featured" applications like:
-#  [✔] File downloader
-#  [✔] Simple photo viewer
-#  [✔] Calculator
-#  [ ] Notepad
 # TODO: Create into proper Python package - https://antonz.org/python-packaging/
 
 __version_info__ = (0, 0, 1)
