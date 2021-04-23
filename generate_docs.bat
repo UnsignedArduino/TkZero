cff3296aaa3de04bcd10c9a9a98ad37d1fb7b1fc
UnsignedArduino <UnsignedArduino@outlook.com>
2021-04-22 20:35:04 -0400

    Move docs under different folder

--- generate_docs.bat
+++ generate_docs.bat
@@ -1,3 +1,5 @@
 :: Will need to install pdoc via:
 :: pip install pdoc3
-pdoc --html TkZero --output-dir Documentation --force
+pdoc --html TkZero --output-dir docs --force
+move docs\TkZero\* docs
+rmdir docs\TkZero
