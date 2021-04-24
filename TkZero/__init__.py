e7a086acfc1494255845ee6cca32359b69e42510
UnsignedArduino <UnsignedArduino@outlook.com>
2021-04-23 20:47:36 -0400

    Upload JSON coverage

--- TkZero/__init__.py
+++ TkZero/__init__.py
@@ -5,7 +5,8 @@ Find this project on PyPI at https://pypi.org/project/TkZero/, or it's source
 code at https://github.com/UnsignedArduino/TkZero.
 """
 # TODO: Create into proper Python package -
-#  https://antonz.org/python-packaging/#a-readme-and-changelog:~:text=A.%20Readme%20and%20changelog
+#  https://antonz.org/python-packaging/#c-cloud-build:~:text=Now%20commit%2C%20push%2C%20and%20enjoy%20the,well%20%2D%20add%20badges%20to%20README.md%3A
+# TODO: Figure out why CI pipeline fails to generate coverage.json properly
 
 __version_info__ = (0, 0, 1)
 __version__ = ""
