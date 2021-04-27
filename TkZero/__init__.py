edadac7461a8453eeb3e4042e6e3355c389c1157
UnsignedArduino <UnsignedArduino@outlook.com>
2021-04-27 08:33:22 -0400

    Fix CI

--- TkZero/__init__.py
+++ TkZero/__init__.py
@@ -6,7 +6,6 @@ code at https://github.com/UnsignedArduino/TkZero.
 """
 # TODO: Create into proper Python package -
 #  https://antonz.org/python-packaging/#c-cloud-build:~:text=Now%20commit%2C%20push%2C%20and%20enjoy%20the,well%20%2D%20add%20badges%20to%20README.md%3A
-# TODO: Figure out why CI pipeline fails to generate coverage.json properly
 
 __version_info__ = (0, 0, 1)
 __version__ = ""
