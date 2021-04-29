45e10d2110fc2bec9ffcbe0bdcab742d4773b7e7
UnsignedArduino <UnsignedArduino@outlook.com>
2021-04-29 16:43:17 -0400

    98% coverage on TkZero/Progressbar

--- Tests/test_Progressbar.py
+++ Tests/test_Progressbar.py
@@ -17,6 +17,20 @@ class ProgressbarTest(unittest.TestCase):
         root.update()
         root.close()
 
+    def test_bad_params(self):
+        root = MainWindow()
+        root.update()
+        with self.assertRaises(TypeError):
+            Progressbar(parent=1)
+        with self.assertRaises(TypeError):
+            Progressbar(root, length=1.1)
+        with self.assertRaises(TypeError):
+            Progressbar(root, length=1, mode=True)
+        with self.assertRaises(TypeError):
+            Progressbar(root, length=1, orientation=lambda: None)
+        root.update()
+        root.close()
+
     def test_good_params(self):
         root = MainWindow()
         root.update()
@@ -33,10 +47,26 @@ class ProgressbarTest(unittest.TestCase):
         p.maximum = 100
         root.update()
         self.assertEqual(p.value, 0)
-        self.assertEqual(p.maximum, 100)
         p.value = 25
         root.update()
         self.assertEqual(p.value, 25)
+        with self.assertRaises(TypeError):
+            p.value = "5"
+        root.close()
+
+    def test_max(self):
+        root = MainWindow()
+        root.update()
+        p = Progressbar(root, orientation=OrientModes.Vertical, mode=ProgressModes.Determinate, length=200)
+        p.grid(row=0, column=0)
+        p.maximum = 100
+        root.update()
+        self.assertEqual(p.maximum, 100)
+        p.maximum = 25
+        root.update()
+        self.assertEqual(p.maximum, 25)
+        with self.assertRaises(TypeError):
+            p.maximum = "5"
         root.close()
 
     def test_indeterminate(self):
@@ -58,6 +88,8 @@ class ProgressbarTest(unittest.TestCase):
         self.assertTrue(p.enabled)
         p.enabled = False
         self.assertFalse(p.enabled)
+        with self.assertRaises(TypeError):
+            p.enabled = "True"
         root.close()
 
 
