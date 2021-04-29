8e4ff918aec15ecba91fd43a16ed4a4067a09a32
UnsignedArduino <UnsignedArduino@outlook.com>
2021-04-29 19:15:47 -0400

    97% coverage on TkZero/Scale

--- Tests/test_Scale.py
+++ Tests/test_Scale.py
@@ -17,6 +17,22 @@ class ScaleTest(unittest.TestCase):
         root.update()
         root.close()
 
+    def test_bad_params(self):
+        root = MainWindow()
+        root.update()
+        with self.assertRaises(TypeError):
+            Scale(parent=1)
+        with self.assertRaises(TypeError):
+            Scale(root, length="5", minimum=0.0, maximum=5.0)
+        with self.assertRaises(TypeError):
+            Scale(root, length=5, minimum=0, maximum=5.0)
+        with self.assertRaises(TypeError):
+            Scale(root, length=5, minimum=0.0, maximum=5)
+        with self.assertRaises(TypeError):
+            Scale(root, length=5, minimum=0.0, maximum=5.0, orientation=[])
+        root.update()
+        root.close()
+
     def test_good_params(self):
         root = MainWindow()
         root.update()
@@ -34,6 +50,8 @@ class ScaleTest(unittest.TestCase):
         s.value = 25
         root.update()
         self.assertEqual(s.value, 25)
+        with self.assertRaises(TypeError):
+            s.value = "foo"
         root.close()
 
     def test_enabled(self):
@@ -45,6 +63,8 @@ class ScaleTest(unittest.TestCase):
         self.assertTrue(s.enabled)
         s.enabled = False
         self.assertFalse(s.enabled)
+        with self.assertRaises(TypeError):
+            s.enabled = "pink cat"
         root.close()
 
 
