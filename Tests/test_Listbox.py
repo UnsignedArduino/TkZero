e5ed402eb2745536f5d16060672f51f1a792f24c
UnsignedArduino <UnsignedArduino@outlook.com>
2021-04-29 11:47:36 -0400

    89% coverage for TkZero/Listbox

--- Tests/test_Listbox.py
+++ Tests/test_Listbox.py
@@ -17,10 +17,29 @@ class ListboxTest(unittest.TestCase):
         root.update()
         root.close()
 
+    def test_bad_params(self):
+        root = MainWindow()
+        root.update()
+        with self.assertRaises(TypeError):
+            Listbox(parent=1)
+        with self.assertRaises(TypeError):
+            Listbox(root, values="")
+        with self.assertRaises(TypeError):
+            Listbox(root, select_mode=1)
+        with self.assertRaises(TypeError):
+            Listbox(root, height=2.5)
+        with self.assertRaises(TypeError):
+            Listbox(root, width="foo")
+        root.update()
+        root.close()
+
     def test_good_params(self):
         root = MainWindow()
         root.update()
-        Listbox(root, width=20, height=10, values=["foo", "bar"]).grid(row=0, column=0)
+        Listbox(root, width=20, height=10, values=["foo", "bar"],
+                on_select=lambda: print("Selected"),
+                on_double_click=lambda: print("Double click")
+                ).grid(row=0, column=0)
         root.update()
         root.close()
 
@@ -33,6 +52,11 @@ class ListboxTest(unittest.TestCase):
         self.assertEqual(l.selected, ())
         l.selected = (0, )
         self.assertEqual(l.selected, (0, ))
+        l.scroll_to(0)
+        with self.assertRaises(TypeError):
+            l.selected = []
+        with self.assertRaises(TypeError):
+            l.scroll_to("")
         root.close()
 
     def test_values(self):
@@ -44,6 +68,8 @@ class ListboxTest(unittest.TestCase):
         self.assertEqual(l.values, [])
         l.values = ["Foo"]
         self.assertEqual(l.values, ["Foo"])
+        with self.assertRaises(TypeError):
+            l.values = ()
         root.close()
 
     def test_enabled(self):
@@ -55,6 +81,8 @@ class ListboxTest(unittest.TestCase):
         self.assertTrue(l.enabled)
         l.enabled = False
         self.assertFalse(l.enabled)
+        with self.assertRaises(TypeError):
+            l.enabled = "boo"
         root.close()
 
 
