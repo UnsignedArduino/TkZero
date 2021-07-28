c7fada1dd7b8a3fabd61e245fb077a1b1c4af40e
UnsignedArduino <UnsignedArduino@outlook.com>
2021-07-28 16:54:23 -0400

    Can import and use themes
    Fixes https://github.com/UnsignedArduino/TkZero/issues/31

--- TkZero/Style.py
+++ TkZero/Style.py
@@ -2,7 +2,10 @@
 Create a theme.
 """
 
+import tkinter as tk
+from pathlib import Path
 from tkinter import ttk
+from typing import Union
 
 
 class WidgetStyleRoots:
@@ -17,6 +20,44 @@ class WidgetStyleRoots:
     Scale = "TScale"
     Scrollbar = "TScrollbar"
     Spinbox = "TSpinbox"
+
+
+def import_theme(root: tk.Tk, path: Union[str, Path]) -> None:
+    """
+    `source` the theme file in Tk. You must call this before using your theme!
+
+    :param root: The root instance of the GUI. Should be a tk.Tk
+    :param path: The path to the theme file. Should be a str or a pathlib.Path
+    :return: None.
+    """
+    if not isinstance(root, tk.Tk):
+        raise TypeError(f"root is not a tk.Tk! (type passed in: {repr(type(root))})")
+    if not isinstance(path, (str, Path)):
+        raise TypeError(
+            f"path is not a str or a pathlib.Path! "
+            f"(type passed in: {repr(type(path))})"
+        )
+    path = str(Path(path).resolve())
+    root.tk.call("source", path)
+
+
+def use_theme(root: tk.Tk, theme: str) -> None:
+    """
+    Use an theme!
+
+    :param root: The root instance of the GUI. Should be a tk.Tk
+    :param theme: The name of the theme, either a built-in one or a theme
+     that was "source"d.
+    :return: None.
+    """
+    if not isinstance(root, tk.Tk):
+        raise TypeError(f"root is not a tk.Tk! (type passed in: {repr(type(root))})")
+    if not isinstance(theme, str):
+        raise TypeError(
+            f"theme is not a str! " f"(type passed in: {repr(type(theme))})"
+        )
+    style = ttk.Style()
+    style.theme_use(theme)
 
 
 def define_style(style_root: str, style_name: str, **kwargs) -> None:
